b7fce8e46fcb45460d2f358a646a9834b3858ad1
Eduardo Stamm <edstamm29@gmail.com>
2021-03-21 13:50:05 -0600

    final commit

--- README.md
+++ README.md
@@ -1,92 +1,46 @@
-# 03 JavaScript: Password Generator
+# Password Generator
 
-## Your Task
-
-This week’s homework requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
-
-The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
-
-## User Story
-
+## Description
 ```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
+This repo contains a random password generator web app. The porpuse of this project is to reenforce the usage of javascript and the logics behind it.
 ```
 
-## Acceptance Criteria
+## Table of Contents 
+- [What to expect](#what-to-expect)
+- [Usage](#usage)
+- [Contribute](#Contribute)
+- [Credits](#credits)
+- [Deployed application](#Deployed-application)
 
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
 
-## Mock-Up
+## What to expect
+In this webpage, you will find:
+- A not editable text box
+- A password generator button
+- Various prompts
 
-The following image shows the web application's appearance and functionality:
 
-![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
+## Usage
+![badmath](https://img.shields.io/github/languages/top/stamm2911/Password-Generator)
+1. When the user clicks the button to generate a password, then a series of prompts are presented for password criteria.
+2. The user is first prompted for password length, the user must only type a length of at least 8 characters and no more than 128 characters.
+3. Then, the user is prompted for lowerCase character types to be included in the password or not.
+4. Then, the user is prompted for upperCase character types to be included in the password or not.
+5. Then, the user is prompted for numeric character types to be included in the password or not.
+6. Then, the user is prompted for special character types to be included in the password or not.
+7. When all prompts are answered, a password is generated that matches the selected criteria.
+8. The password is written to the page in the not editable text box.
 
-## Grading Requirements
 
-This homework is graded based on the following criteria: 
+## Contribute
+Optimize the code for it to run in fewer lines
 
-### Technical Acceptance Criteria: 40%
 
-* Satisfies all of the preceding acceptance criteria plus the following:
+## Credits
+This project was posbile with the knowledge acquired in Tecnologico de Monterrey University Coding Bootcamp https://bootcamp.tec.mx/coding/ 
 
-  * The homework should not produce any errors in the console when you inspect it using Chrome DevTools.
 
-### Deployment: 32%
+## Deployed application
+https://stamm2911.github.io/Password-Generator/
 
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository, with a unique name and a readme describing the project.
-
-- - -
-© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+![Deployed application](img/final-deployed-application.PNG)
--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-//Variables
+//-------Global Variables:-------
 var alfabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var specialsOptions =["|","!","#","$","%","&","/","(",")","=","?","¡","¿"];
+var specialOptions =["|","!","#","$","%","&","/","(",")","=","?","¡","¿","@","+","-","<",">"];
 var numericOptions = [0,1,2,3,4,5,6,7,8,9];
 
-// Assignment Code
+//-------Assignment Code:-------
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword(userInput) {
   var password = generatePassword(userInput);
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
-//Functions:
+//-------Functions:-------
+
+// 1. Generate password function
 function generatePassword(userInput) {
   //Function variables
   var password = [];
   var charactersChosen=0;
   var charactersPerChoiseEl=0;
 
-  // Ask for which choices will be used
+  // Ask user for which choices will be used
   var LowerCase = window.confirm("Do you want Lowercase characters in your password?");
   if(LowerCase){
     charactersChosen++;
@@ -54,7 +55,7 @@ function generatePassword(userInput) {
 
   //If the user choose more than 1 choice, then
   if(charactersChosen != 1){
-    // Gives the first element of charactersPerChoise array a random value from userInput - charactersChosen + 1 
+    // Gives the first element of charactersPerChoise array a random value from userInput to charactersChosen + 1 
     charactersPerChoise[0] = Math.floor(Math.random() * (userInput - charactersChosen + 1)) + 1;
     // Fill the rest of charactersPerChoise array with random numbers considering how many options it has left
     for( var i = 1; i < charactersChosen; i++){
@@ -71,7 +72,7 @@ function generatePassword(userInput) {
       return sumOfElements;
     }
     
-    console.log(charactersPerChoise);
+    console.log("Number of characters per choice: " + charactersPerChoise);
   }else{
     //If the user choose 1 choice, then
     charactersPerChoise[0] = userInput;
@@ -79,7 +80,7 @@ function generatePassword(userInput) {
 
   // The charactersPerChoise array is ramdomly shuffle
   shuffle(charactersPerChoise);
-  console.log(charactersPerChoise);
+  console.log("Number of characters per choice shuffled: " + charactersPerChoise);
 
   // Push ramndom LowerCase characters to password array, the number of them depends on charactersPerChoise array
   if(LowerCase){
@@ -108,14 +109,15 @@ function generatePassword(userInput) {
   // Push ramndom special characters to password array, the number of them depends on charactersPerChoise array
   if(special){
     for(var i=0; i < charactersPerChoise[charactersPerChoiseEl]; i++){
-      password.push(specialsOptions[Math.floor(Math.random() * 10)]);
+      password.push(specialOptions[Math.floor(Math.random() * 18)]);
     }
     charactersPerChoiseEl++;
   }
    
   // Shuffle final password array
-  console.log(password);
+  console.log("Final password array: " + password);
   shuffle(password);
+  console.log("Final password array shuffled: " + password);
   
   
   //Return password to print it
@@ -123,7 +125,7 @@ function generatePassword(userInput) {
   return password.join('');
 }
 
-// Function to call to shuffle an array
+// 2. Function to call to shuffle an array
 function shuffle(array) {
   for (var i = array.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
@@ -133,21 +135,35 @@ function shuffle(array) {
   }
   return array;
 }
-// Add event listener to generate button
+
+//-------Events:-------
+
+// 1. Add event listener to generate button
 generateBtn.addEventListener("click", function () {
   var userInput = window.prompt("Enter the length of your password (min 8 characters, max 128 characters):");
   var checkUserInputNumber = userInput * 0;
   var mod = userInput % 1;
 
+  // If user press cancel then do nothing, close window.prompt & finish the function
+if(userInput === null){
+  return;
+
   // If users input is not a strict number between 8 and 128, then
-  if (isNaN(checkUserInputNumber) || userInput < 8 || userInput > 128 || mod > 0){
+} else if (isNaN(checkUserInputNumber) || userInput < 8 || userInput > 128 || mod > 0){
     while (isNaN(checkUserInputNumber) || userInput < 8 || userInput > 128 || mod > 0) {
-      userInput = window.prompt("Try again. \nEnter the length of your password (min 8 characters, max 128 characters):");
-      mod = userInput % 1;
-      checkUserInputNumber = userInput * 0;
+      // If user press cancel then do nothing, close window.prompt & finish the function
+      if(userInput === null){
+        return;
+      // If user input an invalid value, then ask to try again
+      } else {
+        userInput = window.prompt("Try again. \nEnter the length of your password (min 8 characters, max 128 characters):");
+        mod = userInput % 1;
+        checkUserInputNumber = userInput * 0;
+      }
     }
+    // If user input a valid value, then call function to generate password
     writePassword(userInput);
-  // If users input is a number between 8 and 128, then
+  // If users input is a valid value between 8 and 128, then call function to generate password
   } else {
     console.log("UserInput = " + userInput);
     writePassword(userInput);
